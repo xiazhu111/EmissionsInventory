652e6bef10dfa7f29a354423b0aacc5941050bd3
Alice Zhu <alicexia.zhu@mail.utoronto.ca>
2021-11-30 15:52:03 -0500

    Realized that I needed to put sources on the same line to add them together in the functions. Changed littering uncertainty PDF from gaussian to uniform to avoid negative values. Uncertainties make more sense now - need to show Chelsea and Matt for feedback. nrep = 10,000 good enough; 95% CI within 1% of mean. But histograms do not look smooth so need to improve them most likely

--- inventoryMonteCarlo.R
+++ inventoryMonteCarlo.R
@@ -9,7 +9,7 @@ inventory <- function(n) {
   #sample from the pdfs
   exterior_surface_area <- rtri(n,2530,4382,3578)
   house_shedding <- runif(n,0.01,0.04)
-  littering_rate <- rnorm(n,12656,14271) 
+  littering_rate <- runif(n,1000,33000) #rnorm(n,12656,14271) 
   turf_shedding <- runif(n,0.01,0.04)
   proportion_foam <- runif(n,0.058,0.15)
   pellet_loss <- runif(n,0.0001,0.0004)
@@ -21,26 +21,26 @@ inventory <- function(n) {
   fishinggear_loss <- runif(n,0.01,0.1)
   litres_paint <- runif(n,2,3) 
   #do the calculations
-  sum <- (1112929*exterior_surface_area/100*0.45359237*0.2*house_shedding) #source 1: house paint shedding
-    + (littering_rate) #source 2: littering, illegal dumping, inadequately managed waste, and garbage day overflow
-    + (235.9202662*0.835*0.50) #source 3: road paint shedding
-    + (54*27000*(6.5/1000)*turf_shedding) #source 4: shedding from artificial turf
-    + (95.9971663589714*proportion_foam*3.4) #source 5: foam snow during construction 
-    + (4667*0.0357*pellet_loss*1000) #source 6: pellet spills from plastic industry
-    + (135233*278/10^9) #source 7: shedding of tire dust from airplanes
-    + (1179057*0.86*219*loadperwash*shedding_laundry*0.45/10^9) #source 8: shedding of fibers from laundry
-    + (1179057*0.86*289*loadperdry*shedding_dryer*0.34/10^9) #source 9: fibers from dryers
-    + (1179057*1.3*16000*tire_shedding)/10^9 #source 10: wearing of vehicle tires
-    + (109668*1435.974459546*fishinggear_loss/10^6) #source 11: derelict fishing gear
-    + (7160)*(litres_paint*0.119826404)*(0.01)/1000 #source 12: shedding of paint from aquatic vessels
+ # sum <- (1112929*exterior_surface_area/100*0.45359237*0.2*house_shedding/1000) #source 1: house paint shedding
+   # + (littering_rate) #source 2: littering, illegal dumping, inadequately managed waste, and garbage day overflow
+   # + (235.9202662*0.835*0.50) #source 3: road paint shedding
+   # + (54*27000*(6.5/1000)*turf_shedding) #source 4: shedding from artificial turf
+   # + (95.9971663589714*proportion_foam*3.4) #source 5: foam snow during construction 
+   # + (4667*0.0357*pellet_loss*1000) #source 6: pellet spills from plastic industry
+   # + (135233*278/10^9) #source 7: shedding of tire dust from airplanes
+   # + (1179057*0.86*219*loadperwash*shedding_laundry*0.45/10^9) #source 8: shedding of fibers from laundry
+   # + (1179057*0.86*289*loadperdry*shedding_dryer*0.34/10^9) #source 9: fibers from dryers
+  #  + (1179057*1.3*16000*tire_shedding)/10^9 #source 10: wearing of vehicle tires
+   # + (109668*1435.974459546*fishinggear_loss/10^6) #source 11: derelict fishing gear
+   # + (7160)*(litres_paint*0.119826404)*(0.01)/1000 #source 12: shedding of paint from aquatic vessels
+  sum <- (1112929*exterior_surface_area/100*0.45359237*0.2*house_shedding/1000)+(littering_rate)+(235.9202662*0.835*0.50)+(54*27000*(6.5/1000)*turf_shedding)+(95.9971663589714*proportion_foam*3.4)+(4667*0.0357*pellet_loss*1000)+(135233*278/10^9)+(1179057*0.86*219*loadperwash*shedding_laundry*0.45/10^9)+(1179057*0.86*289*loadperdry*shedding_dryer*0.34/10^9)+(1179057*1.3*16000*tire_shedding)/10^9+(109668*1435.974459546*fishinggear_loss/10^6)+(7160)*(litres_paint*0.119826404)*(0.01)/1000
   #return result
   return(list("sum"=sum))
 }
 #add uncertainty pdf parameters to the list
 param_list = list("n" = 1) #number of arguments matches that of func
-#used value from Steph's code -> MUST RUN THIS BEFORE RUNNING MONTE CARLO SIMULATIONS!
-set.seed(1234)
 #run the actual Monte Carlo simulations
+set.seed(1234) #used value from Steph's code -> MUST RUN THIS BEFORE RUNNING MONTE CARLO SIMULATIONS!
 MC_result <- MonteCarlo(func = inventory, nrep = 100000, param_list = param_list)
 #don't need to generate table. Go straight to making a dataframe in order to plot results
 #visualize results 
@@ -87,7 +87,7 @@ terrestrial <- function(n_terrestrial) {
   #sample from the pdfs
   exterior_surface_area <- rtri(n_terrestrial,2530,4382,3578)
   house_shedding <- runif(n_terrestrial,0.01,0.04)
-  littering_rate <- rnorm(n_terrestrial,12656,14271) 
+  littering_rate <- runif(n_terrestrial,1000,33000) #rnorm(n_terrestrial,12656,14271) 
   turf_shedding <- runif(n_terrestrial,0.01,0.04)
   proportion_foam <- runif(n_terrestrial,0.058,0.15)
   pellet_loss <- runif(n_terrestrial,0.0001,0.0004)
@@ -97,25 +97,15 @@ terrestrial <- function(n_terrestrial) {
   shedding_dryer <- rnorm(n_terrestrial,18,8)
   tire_shedding <- rtri(n_terrestrial,0.05,0.25,0.1)
   #do the calculations
-  sum_terrestrial <- (1112929*exterior_surface_area/100*0.45359237*0.2*house_shedding) #source 1: house paint shedding
-  + (littering_rate) #source 2: littering, illegal dumping, inadequately managed waste, and garbage day overflow
-  + (235.9202662*0.835*0.50) #source 3: road paint shedding
-  + (54*27000*(6.5/1000)*turf_shedding) #source 4: shedding from artificial turf
-  + (95.9971663589714*proportion_foam*3.4) #source 5: foam snow during construction 
-  + (4667*0.0357*pellet_loss*1000) #source 6: pellet spills from plastic industry
-  + (135233*278/10^9) #source 7: shedding of tire dust from airplanes
-  + (1179057*0.86*219*loadperwash*shedding_laundry*0.45/10^9) #source 8: shedding of fibers from laundry
-  + (1179057*0.86*289*loadperdry*shedding_dryer*0.34/10^9) #source 9: fibers from dryers
-  + (1179057*1.3*16000*tire_shedding)/10^9 #source 10: wearing of vehicle tires
+  sum_terrestrial <- (1112929*exterior_surface_area/100*0.45359237*0.2*house_shedding/1000)+(littering_rate)+(235.9202662*0.835*0.50)+(54*27000*(6.5/1000)*turf_shedding)+(95.9971663589714*proportion_foam*3.4)+(4667*0.0357*pellet_loss*1000)+(135233*278/10^9)+(1179057*0.86*219*loadperwash*shedding_laundry*0.45/10^9)+(1179057*0.86*289*loadperdry*shedding_dryer*0.34/10^9)+(1179057*1.3*16000*tire_shedding)/10^9
   #return result
   return(list("sum"=sum_terrestrial))
 }
 #add uncertainty pdf parameters to the list
 param_terrestrial = list("n_terrestrial" = 1) #number of arguments matches that of func
-#ALWAYS RUN SET.SEED BEFORE MONTE CARLO
-set.seed(1234)
 #run the actual Monte Carlo simulations
-MC_terrestrial <- MonteCarlo(func = terrestrial, nrep = 1000, param_list = param_terrestrial)
+set.seed(1234) #ALWAYS RUN SET.SEED BEFORE MONTE CARLO
+MC_terrestrial <- MonteCarlo(func = terrestrial, nrep = 10000, param_list = param_terrestrial)
 #don't need to generate table. Go straight to making a dataframe in order to plot results
 #visualize results 
 #with code from: http://www.sthda.com/english/wiki/ggplot2-histogram-plot-quick-start-guide-r-software-and-data-visualization
@@ -138,17 +128,15 @@ maritime <- function(n) {
   fishinggear_loss <- runif(n,0.01,0.1)
   litres_paint <- runif(n,2,3) 
   #do the calculations
-  sum <- (109668*1435.974459546*fishinggear_loss/10^6) #source 11: derelict fishing gear
-  + (7160)*(litres_paint*0.119826404)*(0.01)/1000 #source 12: shedding of paint from aquatic vessels
+  sum <- (109668*1435.974459546*fishinggear_loss/10^6)+(7160)*(litres_paint*0.119826404)*(0.01)/1000
   #return result
   return(list("sum"=sum))
 }
 #add uncertainty pdf parameters to the list
 param_maritime = list("n" = 1) #number of arguments matches that of func
-#ALWAYS RUN SET.SEED BEFORE MONTE CARLO
-set.seed(1234)
 #run the actual Monte Carlo simulations
-MC_maritime <- MonteCarlo(func = maritime, nrep = 1000, param_list = param_maritime)
+set.seed(1234) #ALWAYS RUN SET.SEED BEFORE MONTE CARLO
+MC_maritime <- MonteCarlo(func = maritime, nrep = 10000, param_list = param_maritime)
 #don't need to generate table. Go straight to making a dataframe in order to plot results
 #visualize results 
 #with code from: http://www.sthda.com/english/wiki/ggplot2-histogram-plot-quick-start-guide-r-software-and-data-visualization
@@ -173,18 +161,15 @@ paint <- function(n) {
   house_shedding <- runif(n,0.01,0.04)
   litres_paint <- runif(n,2,3) 
   #do the calculations
-  sum <- (1112929*exterior_surface_area/100*0.45359237*0.2*house_shedding) #source 1: house paint shedding
-  + (235.9202662*0.835*0.50) #source 3: road paint shedding
-  + (7160)*(litres_paint*0.119826404)*(0.01)/1000 #source 12: shedding of paint from aquatic vessels
+  sum <- (1112929*exterior_surface_area/100*0.45359237*0.2*house_shedding/1000)+(235.9202662*0.835*0.50)+(7160)*(litres_paint*0.119826404)*(0.01)/1000
   #return result
   return(list("sum"=sum))
 }
 #add uncertainty pdf parameters to the list
 param_paint = list("n" = 1) #number of arguments matches that of func
-#ALWAYS RUN SET.SEED BEFORE MONTE CARLO
-set.seed(1234)
 #run the actual Monte Carlo simulations
-MC_paint <- MonteCarlo(func = paint, nrep = 1000, param_list = param_paint)
+set.seed(1234) #ALWAYS RUN SET.SEED BEFORE MONTE CARLO
+MC_paint <- MonteCarlo(func = paint, nrep = 10000, param_list = param_paint)
 #don't need to generate table. Go straight to making a dataframe in order to plot results
 #visualize results 
 #with code from: http://www.sthda.com/english/wiki/ggplot2-histogram-plot-quick-start-guide-r-software-and-data-visualization
@@ -207,18 +192,15 @@ tires <- function(n) {
   #sample from the pdfs
   tire_shedding <- rtri(n,0.05,0.25,0.1)
   #do the calculations
-  sum <- (135233*278/(10^9)) #source 7: shedding of tire dust from airplanes
-  + (1179057*1.3*16000*tire_shedding)/(10^9) #source 10: wearing of vehicle tires
+  sum <- (135233*278/(10^9))+(1179057*1.3*16000*tire_shedding)/(10^9)
   #return result
   return(list("sum"=sum))
 }
-rtri(1,0.05,0.25,0.1)
 #add uncertainty pdf parameters to the list
 param_tires = list("n" = 1) #number of arguments matches that of func
-#ALWAYS RUN SET.SEED BEFORE MONTE CARLO
-set.seed(1234)
 #run the actual Monte Carlo simulations
-MC_tires <- MonteCarlo(func = tires, nrep = 1000, param_list = param_tires)
+set.seed(1234) #ALWAYS RUN SET.SEED BEFORE MONTE CARLO
+MC_tires <- MonteCarlo(func = tires, nrep = 10000, param_list = param_tires)
 #don't need to generate table. Go straight to making a dataframe in order to plot results
 #visualize results 
 #with code from: http://www.sthda.com/english/wiki/ggplot2-histogram-plot-quick-start-guide-r-software-and-data-visualization
@@ -243,24 +225,51 @@ clothes <- function(n) {
   loadperdry <- rnorm(n,0.438,0.017)
   shedding_dryer <- rnorm(n,18,8)
   #do the calculations
-  sum <- (1179057*0.86*219*loadperwash*shedding_laundry*0.45/10^9) #source 8: shedding of fibers from laundry
-  + (1179057*0.86*289*loadperdry*shedding_dryer*0.34/10^9) #source 9: fibers from dryers
+  sum <- (1179057*0.86*219*loadperwash*shedding_laundry*0.45/10^9)+(1179057*0.86*289*loadperdry*shedding_dryer*0.34/10^9)
   #return result
   return(list("sum"=sum))
 }
 #add uncertainty pdf parameters to the list
 param_clothes = list("n" = 1) #number of arguments matches that of func
-#ALWAYS RUN SET.SEED BEFORE MONTE CARLO
-set.seed(1234)
 #run the actual Monte Carlo simulations
-MC_clothes <- MonteCarlo(func = clothes, nrep = 1000, param_list = param_clothes)
+set.seed(1234) #ALWAYS RUN SET.SEED BEFORE MONTE CARLO
+MC_clothes <- MonteCarlo(func = clothes, nrep = 10000, param_list = param_clothes)
 #don't need to generate table. Go straight to making a dataframe in order to plot results
 #visualize results 
 #with code from: http://www.sthda.com/english/wiki/ggplot2-histogram-plot-quick-start-guide-r-software-and-data-visualization
 df_clothes <- MakeFrame(MC_clothes)
 head(df_clothes)
-p <- ggplot(df_clothes, aes(x=sum)) + geom_histogram(binwidth = 1, color = "black") + labs(title = "Histogram of tire dust sums",x="emissions (T)",y="Frequency")
+p <- ggplot(df_clothes, aes(x=sum)) + geom_histogram(binwidth = 1, color = "black") + labs(title = "Histogram of clothes sums",x="emissions (T)",y="Frequency")
 p + geom_vline(xintercept=mean(df_clothes$sum),color="blue",linetype="dashed",size=1) 
 summary(df_clothes$sum)
 norm.interval(df_clothes$sum)
+#
+#
+#
+#
+#
+#
+#littering - troubleshooting 
+littering <- function(n) {
+  #sample from the pdfs
+  littering_rate <- runif(n,1000,33000) 
+  #do the calculations
+  sum <- (littering_rate) #source 2: littering, illegal dumping, inadequately managed waste, and garbage day overflow
+  #return result
+  return(list("sum"=sum))
+}
+param_littering = list("n" = 1) #number of arguments matches that of func
+#ALWAYS RUN SET.SEED BEFORE MONTE CARLO
+set.seed(1234)
+#run the actual Monte Carlo simulations
+MC_littering <- MonteCarlo(func = littering, nrep = 1000, param_list = param_littering)
+#don't need to generate table. Go straight to making a dataframe in order to plot results
+#visualize results 
+#with code from: http://www.sthda.com/english/wiki/ggplot2-histogram-plot-quick-start-guide-r-software-and-data-visualization
+df_littering <- MakeFrame(MC_littering)
+head(df_littering)
+p <- ggplot(df_littering, aes(x=sum)) + geom_histogram(binwidth = 1, color = "black") + labs(title = "Histogram of tire dust sums",x="emissions (T)",y="Frequency")
+p + geom_vline(xintercept=mean(df_littering$sum),color="blue",linetype="dashed",size=1) 
+summary(df_littering$sum)
+norm.interval(df_littering$sum)
 #end
